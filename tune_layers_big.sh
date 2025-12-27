9194db1a778aa6bef058a405ecb86aa741585a1a
Duong Thanh Tran <duongdayne1909@gmail.com>
2025-12-28 05:06:28 +0900

    chore: make some more configs

--- tune_layers_big.sh
+++ tune_layers_big.sh
@@ -1,5 +1,3 @@
-python train.py --config configs/tune_layers_big/fusion_1_geometric_5.yaml --train_all_folds --verbose
-python train.py --config configs/tune_layers_big/fusion_2_geometric_5.yaml --train_all_folds --verbose
-python train.py --config configs/tune_layers_big/fusion_3_geometric_5.yaml --train_all_folds --verbose
-python train.py --config configs/tune_layers_big/fusion_4_geometric_5.yaml --train_all_folds --verbose
-python train.py --config configs/tune_layers_big/fusion_5_geometric_5.yaml --train_all_folds --verbose
+python train.py --config configs/tune_layers_big/fusion_3_geometric_4.yaml --train_all_folds --verbose
+python train.py --config configs/tune_layers_big/fusion_2_geometric_4.yaml --train_all_folds --verbose
+python train.py --config configs/tune_layers_big/fusion_1_geometric_4.yaml --train_all_folds --verbose
