729bba1f0069171bfbf67ae358723236c87f3990
Duong Thanh Tran <duongdayne1909@gmail.com>
2026-01-15 15:33:24 +0900

    chore: add calculate metrics for folds and remove .sh files

--- tune_layers_big.sh
+++
@@ -1,3 +0,0 @@
-python train.py --config configs/tune_layers_big/fusion_3_geometric_4.yaml --train_all_folds --verbose
-python train.py --config configs/tune_layers_big/fusion_2_geometric_4.yaml --train_all_folds --verbose
-python train.py --config configs/tune_layers_big/fusion_1_geometric_4.yaml --train_all_folds --verbose
